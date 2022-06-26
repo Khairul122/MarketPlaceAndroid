c53194f8de4dca4fdb9a3308e5aa940171f67be1
Khairul122 <khairulhuda242@gmail.com>
2022-06-27 00:02:27 +0700

    init

---
+++ app/src/main/java/com/toko/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.toko.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
